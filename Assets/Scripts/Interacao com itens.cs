81ad301276da62bdd065013da1b33def98c7961b
gvieirasantos <gvieirasantos48@gmail.com>
2026-06-17 21:54:31 -0300

    nteração com objetos, primeiros passos pra fazer o spawn funcionar em outras portas e começo de salvar o estado da luz no quarto na transição de cena

---
+++ Assets/Scripts/Interacao com itens.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Interacaocomitens : MonoBehaviour
+{
+    public KeyCode interacaoTecla = KeyCode.G;
+    private bool jogadorNaArea;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created  
+
+    private void OnTriggerEnter2D(Collider2D other)
+    { //detecta quando o jogador entra na área
+        if (other.CompareTag("Player"))
+        {
+            jogadorNaArea = true;
+        }
+
+
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    { //detecta quando o jogador sai da area
+        if (other.CompareTag("Player"))
+        {
+            jogadorNaArea = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jogadorNaArea && Input.GetKeyDown(interacaoTecla))
+        {
+            Debug.Log("Interagiu com o item!");
+            Destroy(gameObject); // Remove o item da cena após a interação
+        }
+    }
+}
