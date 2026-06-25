8d610b4731389a04a272e0365722bad958b8a534
gvieirasantos <gvieirasantos48@gmail.com>
2026-06-25 19:50:23 -0300

    Revert "Começo do inventário"
    This reverts commit 2b917a12701762d839449d058bb788cbc572c17e.

--- Assets/Scripts/Interacao com itens.cs
+++ Assets/Scripts/Interacao com itens.cs
@@ -5,9 +5,6 @@ public class Interacaocomitens : MonoBehaviour
     public KeyCode interacaoTecla = KeyCode.G;
     private bool jogadorNaArea;
 
-    public Sprite iconeItem; 
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created  
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -34,15 +31,6 @@ public class Interacaocomitens : MonoBehaviour
         {
             Debug.Log("Interagiu com o item!");
             Destroy(gameObject); // Remove o item da cena após a interação
-
-            InventarioUI inventario = Object.FindFirstObjectByType<InventarioUI>();
-
-             if (inventario != null)
-            {
-                inventario.AdicionarItem(iconeItem);
-            }
-
-
         }
     }
 }
