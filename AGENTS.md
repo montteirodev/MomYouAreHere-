c9290faa5cd2e21452cd44745469783c140f44ed
gvieirasantos <gvieirasantos48@gmail.com>
2026-06-25 20:32:42 -0300

    Restaura projeto ao estado do commit 18ad66d

--- AGENTS.md
+++
@@ -1,8 +0,0 @@
-<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
-- Project name: MomYouAreHere-
-- Unity version: Unity 6000.3.7f1
-- Active game object:
-  - Name: jogador1
-  - Tag: Player
-  - Layer: Default
-<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/SpawnJogador.cs
+++ Assets/Scripts/SpawnJogador.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class SpawnJogador : MonoBehaviour
 {
-    private Luz ligado;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,17 +12,7 @@ public class SpawnJogador : MonoBehaviour
         {
             GameObject spawn = GameObject.Find("Spawn_PortaSala");
             transform.position = spawn.transform.position;
-
         }
-        if (UltimaPorta == "portaSala" && ligado)
-
-
-            if (UltimaPorta == "portaBanheiro")
-        {
-            GameObject spawn = GameObject.Find("Spawn_PortaBanheiro");
-            transform.position = spawn.transform.position;
-        }
-
 
     }
 
